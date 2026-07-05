7f12a0105c3480b1d61aed2834263de59b39bbd9
adesam08 <adebukolasamuel08@gmail.com>
2026-07-05 18:31:46 +0100

    fixed bugs

--- EventManager/settings.py
+++ EventManager/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = env('SECRET_KEY')
 if ENVIRONMENT == 'development':
     DEBUG = True
 else:
-    DEBUG = False
+    DEBUG = True
 
 ALLOWED_HOSTS = [
     "eventmanager-api-rxyf.onrender.com",
